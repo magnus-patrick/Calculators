0c778751e520870e563e351fc3f75235e289a5b8
Patrick Floirendo <timmyjb08@gmail.com>
2025-06-01 15:27:00 -0700

    Add files via upload

---
+++ tkinterexperiment.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from calculator2 import derivative
+
+window = tk.Tk()
+window.geometry("700x400")
+window.title("Tkinter Experiment")
+
+
+widget = tk.Label(window, text = "Want to do derivatives?", font = ("Times New Roman", 20))
+widget.pack(padx = 35, pady = 20)
+
+def click():
+    widget.config(text = "Okay!\n")
+    derivative()
+
+
+button = tk.Button(window,
+                   text = "Press/Click here!",
+                   command = click,
+                   font = ("Times New Roman", 20))
+button.pack(padx = 50, pady = 70)
+
+
+window.mainloop()
